7b38ad37d000b127b89cfb1014464014c4cd0681
WhiteLeaf03 <2876202234@qq.com>
2023-07-05 17:56:27 +0800

    init

---
+++ src/main/java/top/whiteleaf03/api/WindyApiBackendApplication.java
@@ -0,0 +1,14 @@
+package top.whiteleaf03.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WhiteLeaf03
+ */
+@SpringBootApplication
+public class WindyApiBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WindyApiBackendApplication.class, args);
+    }
+}
