2769574943695f53f585bddfa961aedf0bd6abae
WhiteLeaf03 <2876202234@qq.com>
2023-07-05 20:46:37 +0800

    全局配置类

---
+++ src/main/java/top/whiteleaf03/api/config/GlobalConfig.java
@@ -0,0 +1,29 @@
+package top.whiteleaf03.api.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.io.IOException;
+
+/**
+ * @author WhiteLeaf03
+ */
+@Configuration
+@Data
+@PropertySource(value = "classpath:config.yml", factory = PropertySourceFactoryImpl.class)
+public class GlobalConfig {
+    @Value("${key.slat}")
+    private String slat;
+}
+
+class PropertySourceFactoryImpl implements PropertySourceFactory {
+    @Override
+    public org.springframework.core.env.PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        return new YamlPropertySourceLoader().load(name, resource.getResource()).get(0);
+    }
+}
