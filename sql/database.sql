b169751444c0e8b72993ffc5de1b87a8817a0af5
WhiteLeaf03 <2876202234@qq.com>
2023-07-06 10:59:46 +0800

    数据库文件

---
+++ sql/database.sql
@@ -0,0 +1,25 @@
+drop database if exists windyapi;
+create database windyapi;
+
+use windyapi;
+
+drop table if exists user;
+create table user
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    nickname   varchar(256)                           null comment '用户昵称',
+    account    varchar(256)                           not null comment '账号',
+    avatar     varchar(1024)                          null comment '用户头像',
+    gender     tinyint                                null comment '性别',
+    role       varchar(256) default 'user'            not null comment '用户角色：user / admin',
+    password   varchar(512)                           not null comment '密码',
+    accessKey  varchar(512)                           not null comment 'accessKey',
+    secretKey  varchar(512)                           not null comment 'secretKey',
+    createTime datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint      default 0                 not null comment '是否删除',
+    constraint uni_userAccount
+        unique (account)
+)
+    comment '用户';
