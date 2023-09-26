140061edbf44327714b34df28e71fbb76c3aa0c0
WhiteLeaf03 <2876202234@qq.com>
2023-09-26 11:04:49 +0800

    新增工单记录表 && 格式化

--- sql/database.sql
+++ sql/database.sql
@@ -47,26 +47,40 @@ create table interface_info
 drop table if exists user_interface_record;
 create table if not exists user_interface_record
 (
-    `id`              bigint                               not null auto_increment comment '主键' primary key,
-    `userId`          bigint                               not null comment '调用用户 id',
+    `id`          bigint                               not null auto_increment comment '主键' primary key,
+    `userId`      bigint                               not null comment '调用用户 id',
     `interfaceId` bigint                               not null comment '接口 id',
-    `totalNum`        int        default 0                 not null comment '总调用次数',
-    `leftNum`         int        default 0                 not null comment '剩余调用次数',
-    `status`          tinyint(1) default 0                 not null comment '0-正常，1-禁用',
-    `createTime`      datetime   default CURRENT_TIMESTAMP not null comment '创建时间',
-    `updateTime`      datetime   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    `isDelete`        tinyint    default 0                 not null comment '是否删除(0-未删, 1-已删)'
+    `totalNum`    int        default 0                 not null comment '总调用次数',
+    `leftNum`     int        default 0                 not null comment '剩余调用次数',
+    `status`      tinyint(1) default 0                 not null comment '0-正常，1-禁用',
+    `createTime`  datetime   default CURRENT_TIMESTAMP not null comment '创建时间',
+    `updateTime`  datetime   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `isDelete`    tinyint    default 0                 not null comment '是否删除(0-未删, 1-已删)'
 )
     comment '用户调用接口记录' charset utf8;
 
 drop table if exists user_subscribe_record;
 create table user_subscribe_record
 (
-    `id`              bigint                             not null auto_increment comment '主键' primary key,
-    `userId`          bigint                             not null comment '调用用户 id',
+    `id`          bigint                             not null auto_increment comment '主键' primary key,
+    `userId`      bigint                             not null comment '调用用户 id',
     `interfaceId` bigint                             not null comment '接口 id',
-    `increase`        int                                not null comment '增加的次数',
-    `price`           int                                not null comment '价格',
-    `createTime`      datetime default CURRENT_TIMESTAMP not null comment '创建时间'
+    `increase`    int                                not null comment '增加的次数',
+    `price`       int                                not null comment '价格',
+    `createTime`  datetime default CURRENT_TIMESTAMP not null comment '创建时间'
 )
     comment '用户订阅记录' charset utf8;
+
+drop table if exists `order`;
+create table `order`
+(
+    id            bigint auto_increment
+        primary key,
+    userId        bigint                             not null comment '主键',
+    level         varchar(8)                         not null comment '紧急程度 一般 紧急',
+    submitMessage varchar(1024)                      not null comment '提交内容',
+    replyMessage  varchar(1024)                      not null comment '回复内容',
+    status        tinyint                            not null comment '工单状态 0-尚未处理 1-处理完毕',
+    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '工单记录' charset utf8;
