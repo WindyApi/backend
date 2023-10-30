b644fb6cf43e66d9d6e290c3f75e08ee911fef36
WhiteLeaf03 <whiteleaf03@163.com>
2023-10-30 16:03:22 +0800

    rm 删除Bean名配置

--- src/main/java/top/whiteleaf03/api/WindyApiBackendApplication.java
+++ src/main/java/top/whiteleaf03/api/WindyApiBackendApplication.java
@@ -3,15 +3,12 @@ package top.whiteleaf03.api;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import top.whiteleaf03.api.config.BeanNameConfig;
 
 /**
  * @author WhiteLeaf03
  */
 @EnableDubbo
 @SpringBootApplication
-@ComponentScan(nameGenerator = BeanNameConfig.class)
 public class WindyApiBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(WindyApiBackendApplication.class, args);
